ef711341617cfbc9ad5058c4d924cca0499365b1
Ashish <ashish@workscripts.in>
2016-07-18 12:50:46 +0530

    i n done

--- Routes/routes.js
+++ Routes/routes.js
@@ -2,7 +2,30 @@
     var express  = require('express');
     var app      = express();                               // create our app w/ express
     var mongoose = require('mongoose');
-    var session =require('express-session');
-    var bodyparser = require('body-parser');
+    //var session =require('express-session');
+    var router = express.Router();
+   
     var login = ('./models/login.js');
-    var usercontact = ('./models/usercontact.js')
+    var usercontact = ('./models/usercontact.js');
+
+     router.route('/register').post(function(req, res) {
+
+    	var userlogin = new login({
+          
+          name : "ashish",
+          email: "ashishvmkv",
+          password:'qwerty',
+          mobileno: 9876543211
+
+    	});
+    	
+    	login.save(function(err,data){
+
+    		if(err)
+    			console.log(err);
+    		console.log(data);
+    	})
+    	
+   });
+
+     module.exports =router;
--- app.js
+++ app.js
@@ -1,13 +1,14 @@
   // set up ========================
     var express  = require('express');
     var app      = express();                               // create our app w/ express
-    var mongoose = require('mongoose');                     // mongoose for mongodb
-  
-
+    var mongoose = require('mongoose');   
+    var routes = require('./Routes/routes');
+                  // mongoose for mongodb
+    //var bodyparser = require('body-parser');
 
     // configuration =================
 
-    mongoose.connect('mongodb://localhost/testing', function(err,data)
+    mongoose.connect('mongodb://localhost/contactmanager',function(err,data)
     {
         if(err){
             console.log("kuch toh hai");
@@ -20,6 +21,12 @@
     }
 
    );
+
+    // app.use(bodyParser.json());
+    // app.use(bodyParser.urlencoded({ extended: true }));
+
+   
+     //app.use('/api', routes);
 
     // listen (start app with node server.js) ======================================
     app.listen(5000);
